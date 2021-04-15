9559f7a32259de2ed9b4580151daa8e1ce16f5f1
Kimberly Salas <salask0104@gmail.com>
2021-04-15 16:58:06 -0700

    added font styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -286,5 +286,12 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 5000);
+
+
 // load tasks for the first time
 loadTasks();
